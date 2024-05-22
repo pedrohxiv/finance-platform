98fce4fe56a487070e6d2a7ba6f185c1be87c0c5
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-05-22 14:39:30 -0300

    Implementing transaction actions

---
+++ actions/get-transactions.ts
@@ -0,0 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+
+import { client } from "@/lib/hono";
+
+export const getTransactions = () => {
+  const params = useSearchParams();
+
+  const accountId = params.get("accountId") || "";
+  const from = params.get("from") || "";
+  const to = params.get("to") || "";
+
+  const query = useQuery({
+    queryKey: ["transactions", { accountId, from, to }],
+    queryFn: async () => {
+      const response = await client.api.transactions.$get({
+        query: { accountId, from, to },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch transactions");
+      }
+
+      const { data } = await response.json();
+
+      return data;
+    },
+  });
+
+  return query;
+};
--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -131,6 +131,27 @@ const app = new Hono()
     }
   )
   .post(
+    "/bulk-create",
+    clerkMiddleware(),
+    zValidator("json", z.array(insertTransactionSchema.omit({ id: true }))),
+    async (c) => {
+      const auth = getAuth(c);
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const values = c.req.valid("json");
+
+      const data = await db
+        .insert(transactions)
+        .values(values.map((value) => ({ id: createId(), ...value })))
+        .returning();
+
+      return c.json({ data });
+    }
+  )
+  .post(
     "/bulk-delete",
     clerkMiddleware(),
     zValidator("json", z.object({ ids: z.array(z.string()) })),
